93e9fd39b4f81ab122984736184ac4119f3e98ed
vir-mir <info@vir-mir.ru>
2013-11-27 16:20:50 +1100

    положил в папку чтоб красиво было

--- controllers/main.php
+++ controllers/main.php
@@ -13,7 +13,7 @@ use library\Controller;
 class Main extends Controller{
 
     public function index() {
-        $this->loadTemplate('site/main.twig');
+        $this->loadTemplate('site/main/main.twig');
     }
 
 } 
